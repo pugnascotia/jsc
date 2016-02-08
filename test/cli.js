a91a70c3c1f6439b81bbdd061a99784907a303fc
Rory Hunter <rory.hunter@blackpepper.co.uk>
2016-02-08 12:47:12 +0000

    More test tweaks

--- test/cli.js
+++ test/cli.js
@@ -7,12 +7,26 @@ var cli = require('../lib/cli.js');
 describe('Command line processing', function() {
 
   describe('Running jsc', function() {
-    it('without arguments should print help', function(done) {
+
+    // As these tests involve forking and exec'ing, be a little more
+    // generous about timing.
+    this.slow(500);
+
+    it('should print usage if no arguments are supplied', function(done) {
 
       cp.exec('lib/cli.js', { encoding: 'utf8' }, (error, stdout, stderr) => {
         assert.equal(error.code, 1, 'Exit code mismatch');
         assert.equal(stderr.trim(), 'No input files specified', 'Error message mismatch');
         assert.ok(/Usage: jsc/.test(stdout), 'Usage not printed');
+        done();
+      });
+    });
+
+    it('should complain if an input file doesn\'t exist', function(done) {
+
+      cp.exec('lib/cli.js not_a_file.js', { encoding: 'utf8' }, (error, stdout, stderr) => {
+        assert.equal(error.code, 1, 'Exit code mismatch');
+        assert.equal(stderr.trim(), 'No files to process or files don\'t exist', 'Error message mismatch');
         done();
       });
     });
